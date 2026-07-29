6438618ea1b090afaaeb9b2462bfc42f9d938678
SergiyL <s.lyubchenko@hotmail.com>
2026-07-29 18:12:22 +0000

    rsi indicator

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from src.data.fetcher import get_prices
-from src.indicators.indicators import moving_average_trend  # чи як ти назвав функцію
-
+from src.indicators.indicators import moving_average_trend, rsi
 tickers = ['GOOGL', 'META', 'NFLX']
 dat = get_prices(tickers, '1mo')
 
 for ticker in tickers:
     trend = moving_average_trend(dat.loc[:, ('Close', ticker)], 5)
+    rsi_trend = rsi(dat.loc[:, ('Close', ticker)], 14)
     print(f"\n{ticker}:")
     print(trend)
+    print(rsi_trend)
--- src/indicators/indicators.py
+++ src/indicators/indicators.py
@@ -3,3 +3,14 @@ def moving_average_trend(prices, window):
     prices_clean = prices.loc[roll_mean.index]
     trend = prices_clean > roll_mean
     return trend
+
+def rsi(prices, window):
+    changes = prices.diff()
+    gains = changes.clip(lower=0)
+    losses = -changes.clip(upper=0)
+    avg_gain = gains.rolling(window).mean()
+    avg_loss = losses.rolling(window).mean()
+
+    rs = avg_gain / avg_loss
+    rsi = 100 - (100 / (1 + rs))
+    return rsi
